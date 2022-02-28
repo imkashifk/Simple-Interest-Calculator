0c3e0e1f5383fab4101930646eafebf73515f2e5
Kashif Khan <imkashifk@gmail.com>
2022-02-28 22:00:11 +0000

    New Changes

--- script.js
+++ script.js
@@ -4,19 +4,27 @@ function show_value(x)
 }
 
 function compute(){
-    var amount = parseInt(document.getElementById("principal").value);
-    var rate = document.getElementById("rate").value;
-    var time = parseInt(document.getElementById("mySelect").value);
-    //console.log(amount, rate, time);
-    var interest = (amount * rate * time) / 100;
-    var yearInTheFuture = new Date().getFullYear() + time;
-    //Create the Interest text
-    var total = amount + interest;
-    //var totalTime = 
-    document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + amount + "</mark>,<br/>" +
+    //check whether principal value is empty
+    var principalvalue = document.getElementById("principal").value;
+    if(principalvalue==""){
+    alert("Please Enter a Principal Value");
+    focus.principalvalue;
+    }
+    else {
+        var amount = parseInt(document.getElementById("principal").value);
+        var rate = document.getElementById("rate").value;
+        var time = parseInt(document.getElementById("mySelect").value);
+        //console.log(amount, rate, time);
+        var interest = (amount * rate * time) / 100;
+        var yearInTheFuture = new Date().getFullYear() + time;
+        //Create the Interest text
+        var total = amount + interest;
+        //var totalTime = 
+        document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + amount + "</mark>,<br/>" +
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
         "You will receive an amount of <mark>" + total + "</mark>,<br/> " +
-        "in the year <mark>" + yearInTheFuture + "</mark>"; 
+        "in the year <mark>" + yearInTheFuture + "</mark>";
+    } 
 }
 
 //Check for positive values
